ac9f651ce6d9838ed1de218431a91c13284c7e81
NthPortal <nthportal@gmail.com>
2017-01-05 06:50:27 -0500

    Add documentation

--- src/main/scala/com/nthportal/shell/Command.scala
+++ src/main/scala/com/nthportal/shell/Command.scala
@@ -1,5 +1,11 @@
 package com.nthportal.shell
 
+/**
+  * A command to be executed by a [[Shell]].
+  */
 trait Command extends SubCommand {
+  /**
+    * The name of the command.
+    */
   val name: String
 }
--- src/main/scala/com/nthportal/shell/OutputSink.scala
+++ src/main/scala/com/nthportal/shell/OutputSink.scala
@@ -1,11 +1,40 @@
 package com.nthportal.shell
 
+/**
+  * Something to which output can be written (by a [[Shell]]).
+  */
 trait OutputSink {
+  /**
+    * Writes a string to this output sink.
+    *
+    * @param s the string to be written
+    */
   def write(s: String): Unit
 
+  /**
+    * Writes an object to this output sink.
+    *
+    * @param any the object to be written
+    */
   def write(any: Any): Unit = write(any.toString)
 
+  /**
+    * Writes a string to this output sink as a line.
+    *
+    * It is RECOMMENDED that this be equivalent to writing the string
+    * followed by a newline character.
+    *
+    * @param s the string to be written
+    */
   def writeln(s: String): Unit
 
+  /**
+    * Writes an object to this output sink as a line.
+    *
+    * It is RECOMMENDED that this be equivalent to writing the object
+    * followed by a newline character.
+    *
+    * @param any the object to be written
+    */
   def writeln(any: Any): Unit = writeln(any.toString)
 }
