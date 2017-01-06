1bc46126b9ec8059412cece643fde1a7e1549e90
NthPortal <nthportal@gmail.com>
2017-01-05 22:45:19 -0500

    Add documentation
    Add documentation for Shell.

--- src/main/scala/com/nthportal/shell/Shell.scala
+++ src/main/scala/com/nthportal/shell/Shell.scala
@@ -2,17 +2,51 @@ package com.nthportal.shell
 
 import com.nthportal.shell.internal.ShellCore
 
+/**
+  * A shell for executing commands.
+  */
 sealed trait Shell {
+  /**
+    * Returns the commands which this shell can execute.
+    *
+    * @return the commands which this shell can execute
+    */
   def commands: ImmutableIterable[Command]
 
+  /**
+    * Returns the [[LineParser]] which this shell uses to parse lines.
+    *
+    * @return the LineParser which this shell uses to parse lines
+    */
   def lineParser: LineParser
 
+  /**
+    * Returns a sequence of suggested completions for the final argument
+    * of a line. Returns an empty sequence if no suggestions are available
+    * for a line.
+    *
+    * @param line the line for which to generate suggested completions
+    * @return a sequence of suggested completions for the line
+    */
   def tabComplete(line: String): ImmutableSeq[String]
 
+  /**
+    * Executes a line.
+    *
+    * @param line the line to execute
+    */
   def executeLine(line: String): Unit
 }
 
 object Shell {
+  /**
+    * Creates a shell.
+    *
+    * @param lineParser the [[LineParser]] to use for the shell
+    * @param commands the commands for the shell to execute
+    * @param outputProvider the [[OutputProvider]] for the shell
+    * @return a shell created with the given parameters
+    */
   def apply(lineParser: LineParser,
             commands: ImmutableSeq[Command],
             outputProvider: OutputProvider): Shell = {
