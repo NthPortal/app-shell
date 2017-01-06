ae24678cbc041e11389522acd231a89637704892
NthPortal <nthportal@gmail.com>
2017-01-05 23:01:40 -0500

    Add documentation
    Add documentation for util package.

--- src/main/scala/com/nthportal/shell/util/CommandTabCompleter.scala
+++ src/main/scala/com/nthportal/shell/util/CommandTabCompleter.scala
@@ -1,7 +1,17 @@
 package com.nthportal.shell
 package util
 
+/**
+  * Delegates the tab-completion of a [[Command]] to one of a sequence of commands
+  * based on the arguments with which this is executed. That is, if the first
+  * argument with which the tab-completion was requested is the name of a command
+  * (in [[CommandDelegator.commands]]), it will delegate the tab-completion to
+  * that command with the subsequent arguments.
+  */
 trait CommandTabCompleter extends CommandDelegator with TabCompletable {
+  /**
+    * @inheritdoc
+    */
   override final def tabComplete(args: ImmutableSeq[String]): ImmutableSeq[String] = args match {
     case Seq() => Nil
     case Seq(prefix) =>
