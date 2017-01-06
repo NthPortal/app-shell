41b4716c67275774789c7c0f57b92755edc6b20d
NthPortal <nthportal@gmail.com>
2017-01-05 22:45:36 -0500

    Add documentation
    Add documentation for async package.

--- src/main/scala/com/nthportal/shell/async/InputProvider.scala
+++ src/main/scala/com/nthportal/shell/async/InputProvider.scala
@@ -2,6 +2,19 @@ package com.nthportal.shell.async
 
 import scala.concurrent.Future
 
+/**
+  * Something which provides input (asynchronously) for an [[AsyncShell]].
+  */
 trait InputProvider {
+  /**
+    * Returns a [[Future]] which will contain the next [[InputAction action]]
+    * to be executed (by an [[AsyncShell]]).
+    *
+    * Successive invocations of this method MUST NOT return Futures which will
+    * be completed with the same action; they MUST return Futures which will be
+    * completed with successive requested actions.
+    *
+    * @return a Future which will contain the next action to be executed
+    */
   def nextAction: Future[InputAction[_]]
 }
