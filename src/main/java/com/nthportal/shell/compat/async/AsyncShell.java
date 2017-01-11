b80396442f0079ca4784a8591857052225a93202
NthPortal <nthportal@gmail.com>
2017-01-11 02:13:10 -0500

    Add comments
    Add comments for Java interfaces in compat.async.

--- src/main/java/com/nthportal/shell/compat/async/AsyncShell.java
+++ src/main/java/com/nthportal/shell/compat/async/AsyncShell.java
@@ -6,13 +6,41 @@ import scala.concurrent.Future;
 
 import java.util.concurrent.CompletionStage;
 
+/**
+ * An asynchronous shell. It processes commands and tab-completions asynchronously.
+ */
 public interface AsyncShell {
+    /**
+     * Terminates this asynchronous shell so that it will no longer process inputs.
+     * It cannot be terminated more than once.
+     *
+     * Note: Invoking this method does NOT interrupt an action which is already
+     * being processed.
+     *
+     * @return a {@link CompletionStage} which will be completed once the shell is fully terminated
+     */
     default CompletionStage<Void> terminate() {
         return FutureConverters.toJava(terminate0());
     }
 
+    /**
+     * Terminates this asynchronous shell so that it will no longer process inputs.
+     * It cannot be terminated more than once.
+     *
+     * Note: Invoking this method does NOT interrupt an action which is already
+     * being processed.
+     *
+     * @return a Future which will be completed once the shell is fully terminated
+     */
     Future<Void> terminate0();
 
+    /**
+     * Creates an asynchronous shell.
+     *
+     * @param inputProvider the {@link InputProvider} for the asynchronous shell
+     * @param shell         the (synchronous) shell to be managed asynchronously by the AsyncShell
+     * @return an asynchronous shell created with the given parameters
+     */
     static AsyncShell create(InputProvider inputProvider, Shell shell) {
         return AsyncShellImpl.apply(inputProvider, shell);
     }
--- src/main/java/com/nthportal/shell/compat/async/InputAction.java
+++ src/main/java/com/nthportal/shell/compat/async/InputAction.java
@@ -7,19 +7,55 @@ import scala.concurrent.Future;
 import java.util.List;
 import java.util.concurrent.CompletionStage;
 
+/**
+ * An action requested to be executed by a {@link Shell} by the
+ * {@link InputProvider input source} of an {@link AsyncShell}.
+ *
+ * @param <T> the type of the result of the action
+ */
 public interface InputAction<T> {
+    /**
+     * Returns a {@link CompletionStage} which will contain the result of this action.
+     *
+     * @return a CompletionStage which will contain the result of this action
+     */
     default CompletionStage<T> completionStage() {
         return FutureConverters.toJava(future());
     }
 
+    /**
+     * Returns a {@link Future} which will contain the result of this action.
+     *
+     * @return a Future which will contain the result of this action
+     */
     Future<T> future();
 
+    /**
+     * Returns the result of an action performed with a {@link Shell}.
+     *
+     * @param shell the shell with which to perform the action
+     * @return the result of the action
+     */
     T action(Shell shell);
 
+    /**
+     * Creates an input action for tab-completing a line.
+     *
+     * @param line the line to be tab-completed
+     * @return an input action for tab-completing the given line
+     * @see Shell#tabComplete(String)
+     */
     static InputAction<List<String>> tabCompletion(String line) {
         return JCompatInputAction.tabCompletion(line);
     }
 
+    /**
+     * Creates an input action for executing a line.
+     *
+     * @param line the line to be executed
+     * @return an input action for executing the given line
+     * @see Shell#executeLine(String)
+     */
     static InputAction<Void> execution(String line) {
         return JCompatInputAction.execution(line);
     }
